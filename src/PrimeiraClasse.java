423ede941e514a52fed7a54020188fcbf7c2b5ea
Thyago <thyagoborgescarvalho@gmail.com>
2026-06-20 21:22:18 -0300

    Hello world de java e condicional

---
+++ src/PrimeiraClasse.java
@@ -0,0 +1,11 @@
+public class PrimeiraClasse {
+    public static void main (String[] args){
+
+        System.out.println("Conclui a primeira aula");
+        double media = (9.8 + 7 + 6.5)/3;
+
+        System.out.println(media);
+
+
+    }
+}
---
+++ src/condicional.java
@@ -0,0 +1,15 @@
+public class condicional  {
+    public static void main  ( String[] args){
+
+        int anoDeLancamento = 1990;
+
+
+    if (anoDeLancamento >= 2026){
+        System.out.println("M1");
+
+    } else {
+        System.out.println("M2");
+    }
+
+    }
+}
